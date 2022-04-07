34dfdf445f2e28749f3ac8b16e36142b51a699b5
Sivaram01 <sivaram.v10@gmail.com>
2022-04-07 14:37:42 +0530

    My portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+         Hola, Welcome <br/> I'm Sivaram Vasudevan <br />
+          Full  Stack Developer
+        </SectionTitle>
+        <SectionText>
+          Full-Stack-developer / Front-End-Developer / UI -Desgin
+        </SectionText>
+        <Button onClick={props.handleClick}>Hire Me</Button>
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: 'Movie Rating App',
+    description: "Using React, Node.js, Express & MongoDB I have Build an Movie Rating Application where user can Add, Edit or Delete the movie, user can also give Ratings Likes and Dislikes to movies and also watch the trailer of that movie by clicking the movie info and Cool part is user can Switch Light Mode and Dark Mode according to their wish.",
+      image: '/images/movie.png',
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://movie-rating-application.netlify.app/',
+    code: 'https://github.com/Sivaram01/Movie-Rating-App',
+    id: 0,
+  },
+  {
+    title: 'Tshirt Kart',
+    description:"Tshirt Kart is an E-commerance Application  where user can signup and start shopping, This App is build using react on Front-end, node & express & mongoose on Backend, Tshirt Kart is completely loaded with Authentication, Authorization, Admin Pannel and Two PAYMENT GATEWAYS, This is a Full Stack MERN Application comes With Braintree and Stripe Payment Methods",
+    image: '/images/tshirt.png',
+    tags: ['React', 'Node' , 'Mongoose' , 'Express'],
+    source: 'https://tshirt-kart.netlify.app/',
+    code: 'https://github.com/Sivaram01/projbackend-Tshirt-Kart',
+    id: 1,
+  },
+  {
+    title: 'Our_Camp_Ground',
+    description: "Our Camp Ground  is a travel site where user can checkout famous Campgrouonds and if your are a traveller you can post New Campgrounds and share your experience leave Ratings and Comments by Logging In.",
+      image: '/images/yelpcamp.png',
+      tags: ['Javascrpit', 'Node' , 'mongoose'],
+    source: 'https://our-camping-ground.herokuapp.com/',
+    code: 'https://github.com/Sivaram01/Camping-Ground',
+    id: 2,
+  },
+  {
+    title: 'Netflix Landing Page',
+    description: "This is a simple Clone of Netflix Landing Page App build With Custom Css,Javascript and Bootstrap ",
+    image: '/images/netflix.png',
+    tags: ['Javascript', 'Bootstrap', 'Css'],
+    source: 'https://netflixlandingclone.netlify.app/',
+    code: 'https://github.com/Sivaram01/netflix-landing-page',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: 'Started my jouney into web development', },
+  { year: 2020, text: 'Build and Shared my projects with the world', },
+  { year: 2021, text: 'Started to learn and Bulid MERN Applications', },
+  { year: 2022, text: 'Started to learn the tools like Figma', },
+];
