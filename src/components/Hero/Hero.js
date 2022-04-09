4a54b0f5ceef41e49f9d3d811ff0a6e547c14208
Sivaram01 <sivaram.v10@gmail.com>
2022-04-09 18:27:59 +0530

    Try Updated Resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,7 @@ const Hero = (props) => (
           Full-Stack-developer / Front-End-Developer / UI -Desgin
         </SectionText>
         <Button  onClick={props.handleClick}>
-          <a href="sivaram-resume.pdf" download='sivaram-resume.pdf'> Resume</a>
+          <a href="sivaram-Resume.pdf" download='sivaram-Resume.pdf'> Resume</a>
         </Button>
       </LeftSection>
     </Section>
